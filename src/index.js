8908e1989c005b7406c9ac6674d41e5eedce1e40
chemimartinez10 <jmmartineztuarez@gmail.com>
2020-07-09 18:03:38 -0400

    servidor creado, estilos agregados, rutas y navegacion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('server working')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+//settings
+app.set('port', 4000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+//routes
+
+app.use(require('./routes/'))
+
+//middlewares
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//DEV server listening
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`)
+})
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.render('index.html', {title: 'Welcome'})
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', {title: 'Contact page'})
+})
+
+module.exports= router
